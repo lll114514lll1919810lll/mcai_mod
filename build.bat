f2c2c10d45177a27a82c3db1e541904b6ffb6c73
ym lee <lym123727@hust.edu.cn>
2026-06-11 12:07:08 +0800

    Sync build.bat: force checkout

--- build.bat
+++ build.bat
@@ -69,7 +69,7 @@ echo JDK: !JAVA_HOME_DIR!
 
 REM Switch branch
 echo Switching to branch !BRANCH!...
-git checkout !BRANCH! 2>nul
+git checkout -f !BRANCH! 2>nul
 if !ERRORLEVEL! NEQ 0 (
     echo Error: Failed to switch to branch !BRANCH!
     exit /b 1

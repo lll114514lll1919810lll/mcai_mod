a43d235b26046467146548e92bf4f940e72ed22c
ym lee <lym123727@hust.edu.cn>
2026-06-17 15:19:51 +0800

    fix: config screen cleanup
    - Remove prompt language setting from GUI
    - Fix tooltips: API fields and prompt paths now have specific tooltips
    - Update reload tip to reflect auto-reload (no more manual /aireload)

--- src/main/java/com/example/mcai/client/config/MCAIConfigScreen.java
+++ src/main/java/com/example/mcai/client/config/MCAIConfigScreen.java
@@ -36,7 +36,7 @@ public class MCAIConfigScreen extends Screen {
     private EditBox maxTokensField, tempField, ctxField, thinkingField, toolCallsField;
     private EditBox sysPromptPathField, reviewPromptPathField;
     private EditBox reviewIntervalField, yellowCardField, redCardField;
-    private EditBox scoreRecoveryField, approvalTimeoutField, promptLangField;
+    private EditBox scoreRecoveryField, approvalTimeoutField;
     private Button chatBtn, cmdBtn, strictBtn, autoReviewBtn;
 
     public MCAIConfigScreen(Screen parent) {
@@ -81,8 +81,8 @@ public class MCAIConfigScreen extends Screen {
 
         addRenderableWidget(new StringWidget(lx, ry(r), LABEL_W, 20,
                 Component.translatable("mcai.config.group.api"), font)); r++;
-        addRow(lx, inX, r, "mcai.config.api_endpoint", apiEndpointField = f(inX, ry(r), kw, gs("apiEndpoint", ""))); r++;
-        addRow(lx, inX, r, "mcai.config.api_key",      apiKeyField     = f(inX, ry(r), kw, gs("apiKey", ""))); r++;
+        addRow(lx, inX, r, "mcai.config.api_endpoint", apiEndpointField = ft(inX, ry(r), kw, gs("apiEndpoint", ""), "mcai.config.tip.api_endpoint")); r++;
+        addRow(lx, inX, r, "mcai.config.api_key",      apiKeyField     = ft(inX, ry(r), kw, gs("apiKey", ""), "mcai.config.tip.api_key")); r++;
         addRow(lx, inX, r, "mcai.config.model",        modelField      = f(inX, ry(r), fw, gs("model", "deepseek-v4-flash"))); r++;
 
         addRenderableWidget(new StringWidget(lx, ry(r), LABEL_W, 20,
@@ -108,9 +108,8 @@ public class MCAIConfigScreen extends Screen {
 
         addRenderableWidget(new StringWidget(lx, ry(r), LABEL_W, 20,
                 Component.translatable("mcai.config.group.prompts"), font)); r++;
-        addRow(lx, inX, r, "mcai.config.sys_prompt_path",    sysPromptPathField    = f(inX, ry(r), kw, gs("systemPromptPath", ""))); r++;
-        addRow(lx, inX, r, "mcai.config.review_prompt_path", reviewPromptPathField = f(inX, ry(r), kw, gs("reviewPromptPath", ""))); r++;
-        addRow(lx, inX, r, "mcai.config.prompt_language",    promptLangField       = f(inX, ry(r), 80, gs("promptLanguage", "zh_cn"))); r++;
+        addRow(lx, inX, r, "mcai.config.sys_prompt_path",    sysPromptPathField    = ft(inX, ry(r), kw, gs("systemPromptPath", ""), "mcai.config.tip.prompt_path")); r++;
+        addRow(lx, inX, r, "mcai.config.review_prompt_path", reviewPromptPathField = ft(inX, ry(r), kw, gs("reviewPromptPath", ""), "mcai.config.tip.prompt_path")); r++;
 
         // Bottom bar: always added last so it renders on top
         int by = height - 25;
@@ -133,7 +132,12 @@ public class MCAIConfigScreen extends Screen {
     private EditBox f(int x, int y, int w, String val) {
         EditBox e = new EditBox(font, x, y, w, 20, Component.literal(""));
         e.setMaxLength(1024); e.setValue(val);
-        e.setTooltip(Tooltip.create(Component.translatable("mcai.config.prompt_tooltip")));
+        return e;
+    }
+
+    private EditBox ft(int x, int y, int w, String val, String tooltipKey) {
+        EditBox e = f(x, y, w, val);
+        e.setTooltip(Tooltip.create(Component.translatable(tooltipKey)));
         return e;
     }
 
@@ -182,7 +186,6 @@ public class MCAIConfigScreen extends Screen {
             o.addProperty("approvalTimeoutMinutes", pi(approvalTimeoutField.getValue(), 10));
             o.addProperty("systemPromptPath", sysPromptPathField.getValue());
             o.addProperty("reviewPromptPath", reviewPromptPathField.getValue());
-            o.addProperty("promptLanguage", promptLangField.getValue());
             try (Writer w = Files.newBufferedWriter(configPath)) { GSON.toJson(o, w); }
             statusWidget.setMessage(Component.translatable("mcai.config.saved"));
             minecraft.setScreen(parent);
